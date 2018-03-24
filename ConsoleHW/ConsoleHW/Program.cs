f6f17f7494d438f12a38857b012f48bb5247f924
bmoolman <bruce.moolman@se.abb.com>
2018-03-24 20:30:44 +0100

    Typo fixed

--- ConsoleHW/ConsoleHW/Program.cs
+++ ConsoleHW/ConsoleHW/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleHW
         static void Main(string[] args)
         {
           Console.WriteLine("Hello World!");
-          Console.ReadLin();
+          Console.ReadLine();
         }
     }
 }
