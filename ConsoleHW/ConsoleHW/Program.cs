708034b49c8340574690725de67f2e1863b971ac
bmoolman <bruce.moolman@se.abb.com>
2018-03-24 19:53:29 +0100

    Adding using statement and exclamation marks in message.

--- ConsoleHW/ConsoleHW/Program.cs
+++ ConsoleHW/ConsoleHW/Program.cs
@@ -6,9 +6,8 @@ namespace ConsoleHW
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Hello World");
-
+          Console.WriteLine("Hello World!");
+            
         }
     }
 }
